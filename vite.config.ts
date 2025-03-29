47810c6213569c73ba91547fd34d28d8f779505b
kobaken <kentafly88@gmail.com>
2025-03-29 09:53:46 +0900

    Added Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import { defineConfig } from 'vite'
 import ssrHotReload from 'vite-plugin-ssr-hot-reload'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [ssrHotReload(), cloudflare()]
+  plugins: [ssrHotReload(), cloudflare(), tailwindcss()],
 })
