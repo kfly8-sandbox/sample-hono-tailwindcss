fd021161da39b5ad9ebe4fed5d8949011056f761
kobaken <kentafly88@gmail.com>
2025-03-29 11:56:51 +0900

    viteで立ち上げているかどうかで切り替え

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,15 +1,13 @@
-import { jsxRenderer,useRequestContext } from 'hono/jsx-renderer'
+import { jsxRenderer } from 'hono/jsx-renderer'
 
 export const renderer = jsxRenderer(({ children }) => {
-  const c = useRequestContext()
-
   return (
     <html>
       <head>
-        {c.env.MODE === 'client' ? (
-          <link href="/static/style.css" rel="stylesheet" />
-        ) : (
+        {!!import.meta.env ? (
           <link href="/src/style.css" rel="stylesheet" />
+        ) : (
+          <link href="/static/style.css" rel="stylesheet" />
         )}
       </head>
       <body>{children}</body>
