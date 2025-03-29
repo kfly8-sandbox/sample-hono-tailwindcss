bbbedac5d00c00b41af9ddc95576f653746e9b7c
kobaken <kentafly88@gmail.com>
2025-03-29 11:40:05 +0900

    Added client mode

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,10 +1,16 @@
-import { jsxRenderer } from 'hono/jsx-renderer'
+import { jsxRenderer,useRequestContext } from 'hono/jsx-renderer'
 
 export const renderer = jsxRenderer(({ children }) => {
+  const c = useRequestContext()
+
   return (
     <html>
       <head>
-        <link href="/src/style.css" rel="stylesheet" />
+        {c.env.MODE === 'client' ? (
+          <link href="/static/style.css" rel="stylesheet" />
+        ) : (
+          <link href="/src/style.css" rel="stylesheet" />
+        )}
       </head>
       <body>{children}</body>
     </html>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,28 @@ import { defineConfig } from 'vite'
 import ssrHotReload from 'vite-plugin-ssr-hot-reload'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [ssrHotReload(), cloudflare(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      plugins: [ tailwindcss() ],
+      build: {
+        rollupOptions: {
+          input: {
+            style: "./src/style.css",
+          },
+          output: {
+            dir: "./dist",
+            assetFileNames: "static/[name].[ext]",
+          },
+        },
+        copyPublicDir: false,
+        minify: true,
+      }
+    }
+  }
+  else {
+    return {
+      plugins: [ssrHotReload(), cloudflare(), tailwindcss()],
+    }
+  }
 })
